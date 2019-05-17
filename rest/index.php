e721848fcb9a714bf054b6ed2005a18bcb89e0cb
Dino Keco <dino.keco@gmail.com>
2019-05-17 10:41:39 +0200

    cars CRUD

--- rest/index.php
+++ rest/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
-Flight::register('db', 'PDO', array('mysql:host=srv10.domenice.net;dbname=biznet_crs','biznet_crs','Z[&QT4jXw[{?'));
+Flight::register('db', 'PDO', array('mysql:host=srv10.domenice.net;dbname=biznet_cars','biznet_cars','a}91g]27#&%~'));
 
 Flight::route('/', function(){
     echo 'hello world!';
@@ -14,9 +14,33 @@ Flight::route('GET /cars', function(){
 
 Flight::route('POST /cars', function(){
     $request = Flight::request()->data->getData();
-    $insert = "INSERT INTO cars (name, power, year, fuel, ccm) VALUES(:name, :power, :year, :fuel, :ccm)";
-    $stmt= Flight::db()->prepare($insert);
-    $stmt->execute($request);
+    if (isset($request['id']) && $request['id'] !=''){
+      $update = "UPDATE cars SET name= :name, power = :power, year = :year, fuel = :fuel, ccm = :ccm WHERE id=:id";
+      $stmt= Flight::db()->prepare($update);
+      $stmt->execute($request);
+      Flight::json(['message' => "Car ".$request['name']." has been updated successfully"]);
+    }else{
+      unset($request['id']);
+      $insert = "INSERT INTO cars (name, power, year, fuel, ccm) VALUES(:name, :power, :year, :fuel, :ccm)";
+      $stmt= Flight::db()->prepare($insert);
+      $stmt->execute($request);
+      Flight::json(['message' => "Car ".$request['name']." has been added successfully"]);
+    }
+});
+
+Flight::route('DELETE /cars/@id', function($id){
+  $query = "DELETE FROM cars WHERE id = :id";
+  $stmt= Flight::db()->prepare($query);
+  $stmt->execute(['id' => $id]);
+  Flight::json(['message' => "Car has been deleted successfully"]);
+});
+
+Flight::route('GET /cars/@id', function($id){
+  $query = "SELECT * FROM cars WHERE id = :id";
+  $stmt= Flight::db()->prepare($query);
+  $stmt->execute(['id' => $id]);
+  $car = $stmt->fetch(PDO::FETCH_ASSOC);
+  Flight::json($car);
 });
 
 Flight::start();
